b7d252801ebd30fdab429e1745ce3ce7858b1c41
Əkbərov Aydin <aydinakbar2007@gmail.com>
2026-02-28 16:02:31 +0400

    Update Program.cs

--- Lesson5_asp-hw/Program.cs
+++ Lesson5_asp-hw/Program.cs
@@ -3,30 +3,20 @@ using Lesson5_asp_hw.Services.Concretes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddScoped<IRepository, ProductRepository>();
-
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-{
     app.UseDeveloperExceptionPage();
-}
 
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseEndpoints(endpoint =>
-{
-    endpoint.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Product}/{action=Index}/{id?}");
-});
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Product}/{action=Index}/{id?}"
+);
 
 app.Run();
-
